fca0abdcd0e3c4836bcf571263aa8edd76494a28
satub <satu.barnhill@gmail.com>
2016-08-19 17:08:20 -0400

    Add barebones for application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "tietoturvaa_junille"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+end
