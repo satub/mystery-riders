c96e65b680a3b9580b2692e654988d76021cf4b3
satub <satu.barnhill@gmail.com>
2016-08-20 15:34:16 -0400

    Added image files for subway line logos

--- app/models/ride_feedback.rb
+++ app/models/ride_feedback.rb
@@ -4,5 +4,6 @@ class RideFeedback < ActiveRecord::Base
 
   # validates_presence_of :passenger_id, :train_id
   validates_length_of :content, minimum: 10, too_short: "Your feedback is too short. Please elaborate on your experience."
-  
+  validates_numericality_of :rating, allow_nil: true, less_than_or_equal_to: 3, message: "Fuhgeddaboutit! Maximum rating for NYC subway lines is 3!"
+
 end
